47fb25236043cd9360eab9a4208cb0a09e2792f4
log.r <log.r@live.cn>
2018-03-16 23:58:28 +0800

    config

--- taskcenter-biz/src/test/java/cn/itbat/whh/taskcenter/service/impl/CmUserTaskServiceImplTest.java
+++ taskcenter-biz/src/test/java/cn/itbat/whh/taskcenter/service/impl/CmUserTaskServiceImplTest.java
@@ -1,9 +1,14 @@
 package cn.itbat.whh.taskcenter.service.impl;
 
 import cn.itbat.whh.taskcenter.BaseTest;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class CmUserTaskServiceImplTest extends BaseTest {
 
+    @Test
+    public void test() {
+
+    }
 }
